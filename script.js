5464a0434b54eff367200b4ca0b980bc4844f93a
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-12 12:03:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 function insert_Row() {
     //Write your code here
+	//getting element by ID using querySelector
 	let existing_Table = document.querySelector("#sampleTable");
+	
+	//creating new row element
 	let new_Row = document.createElement ("tr");
-	existing_Table.prepend(new_Row);
 
+	//creating table data element
 	let new_Cell1 = document.createElement("td");
 	let new_Cell2 = document.createElement("td");
 
-	new_Cell1.innerText = "New Cell 1";
-	new_Cell2.innerText = "New Cell 2";
-	
+	//adding text in td element
+	new_Cell1.innerText = "New Cell1";
+	new_Cell2.innerText = "New Cell2";
 
+	//adding cells in row
 	new_Row.appendChild(new_Cell1);
 	new_Row.appendChild(new_Cell2);
+
+	//adding new row at start of the table.
+	existing_Table.prepend(new_Row);
 }
